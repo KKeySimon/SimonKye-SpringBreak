7ddae85879657ef0789bbb3caba000c65773bdba
Simon Kye <kkeysimon@gmail.com>
2024-03-16 21:47:53 -0400

    shake

--- app/src/main/java/com/example/simonkye_springbreak/MainActivity.kt
+++ app/src/main/java/com/example/simonkye_springbreak/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.simonkye_springbreak
 
 import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.provider.MediaStore.Audio.Radio
 import android.speech.RecognizerIntent
 import android.util.AttributeSet
+import android.view.GestureDetector
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,11 +21,17 @@ import android.widget.Toast
 import com.example.simonkye_springbreak.databinding.ActivityMainBinding
 import java.util.Locale
 import java.util.Objects
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private val REQUEST_CODE_SPEECH_INPUT = 1
+
+    private var sensorManager: SensorManager? = null
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,33 +40,29 @@ class MainActivity : AppCompatActivity() {
 
         // Source: https://www.geeksforgeeks.org/speech-to-text-application-in-android-with-kotlin/
         binding.languageRadioGroup.setOnCheckedChangeListener { _, checkedId ->
-            val selectedLang = findViewById<RadioButton>(checkedId).text.toString()
+            val langCode = when (findViewById<RadioButton>(checkedId).text.toString()) {
+                "English" -> "en"
+                "Korean" -> "kr"
+                "Chinese" -> "zh"
+                else -> "en"
+            }
             val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
 
-            // on below line we are passing language model
-            // and model free form in our intent
             intent.putExtra(
                 RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
             )
 
-            // on below line we are passing our
-            // language as a default language.
             intent.putExtra(
                 RecognizerIntent.EXTRA_LANGUAGE,
-                selectedLang
+                langCode
             )
 
-            // on below line we are specifying a prompt
-            // message as speak to text on below line.
             intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something in the selected language")
-            // on below line we are specifying a try catch block.
-            // in this block we are calling a start activity
-            // for result method and passing our result code.
+
             try {
                 startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
             } catch (e: Exception) {
-                // on below line we are displaying error message in toast
                 Toast
                     .makeText(
                         this@MainActivity, " " + e.message,
@@ -64,23 +71,59 @@ class MainActivity : AppCompatActivity() {
                     .show()
             }
         }
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        Objects.requireNonNull(sensorManager)!!
+            .registerListener(sensorListener, sensorManager!!
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+
+        acceleration = 10f
+        currentAcceleration = SensorManager.GRAVITY_EARTH
+        lastAcceleration = SensorManager.GRAVITY_EARTH
     }
+
+    private val sensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+
+            // Fetching x,y,z values
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            lastAcceleration = currentAcceleration
+
+            // Getting current accelerations
+            // with the help of fetched x,y,z values
+            currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.9f + delta
+
+            // Display a Toast message if
+            // acceleration value is over 12
+            if (acceleration > 12) {
+                Toast.makeText(applicationContext, "Shake event detected", Toast.LENGTH_SHORT).show()
+            }
+        }
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager?.registerListener(sensorListener, sensorManager!!.getDefaultSensor(
+            Sensor .TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager!!.unregisterListener(sensorListener)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
-        // in this method we are checking request
-        // code with our result code.
         if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
-            // on below line we are checking if result code is ok
             if (resultCode == RESULT_OK && data != null) {
-
-                // in that case we are extracting the
-                // data from our array list
                 val res: ArrayList<String> =
                     data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS) as ArrayList<String>
-
-                // on below line we are setting data
-                // to our output text view.
                 binding.editText.setText(
                     Objects.requireNonNull(res)[0]
                 )
