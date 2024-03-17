f77e6e2542fc63e8e11d37c105dcb3741efc9e2c
Simon Kye <kkeysimon@gmail.com>
2024-03-16 23:15:11 -0400

    Assignment Q2 Completed

--- app/src/main/java/com/example/simonkye_springbreak/MainActivity.kt
+++ app/src/main/java/com/example/simonkye_springbreak/MainActivity.kt
@@ -7,25 +7,21 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.provider.MediaStore.Audio.Radio
 import android.speech.RecognizerIntent
-import android.util.AttributeSet
-import android.view.GestureDetector
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.speech.tts.TextToSpeech
 import android.widget.RadioButton
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.simonkye_springbreak.databinding.ActivityMainBinding
 import java.util.Locale
 import java.util.Objects
 import kotlin.math.sqrt
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var textToSpeech: TextToSpeech
 
     private val REQUEST_CODE_SPEECH_INPUT = 1
 
@@ -38,6 +34,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Source: https://www.geeksforgeeks.org/how-to-convert-text-to-speech-in-android/
+        textToSpeech = TextToSpeech(applicationContext) { i ->
+            if (i != TextToSpeech.ERROR) {
+                textToSpeech.setLanguage(Locale.UK)
+            }
+        }
 
         // Source: https://www.geeksforgeeks.org/speech-to-text-application-in-android-with-kotlin/
         binding.languageRadioGroup.setOnCheckedChangeListener { _, checkedId ->
@@ -101,12 +104,28 @@ class MainActivity : AppCompatActivity() {
             if (acceleration > 10) {
                 val checkedButton = binding.languageRadioGroup.checkedRadioButtonId
                 if (checkedButton != -1) {
-                    val coordinate = when (findViewById<RadioButton>(checkedButton).text.toString()) {
-                        "English" -> Pair(51.51, 0.13)
-                        "Korean" -> Pair(37.55, 126.99)
-                        "Chinese" -> Pair(39.9, 116.41)
-                        else -> Pair(0, 0)
+                    val coordinate : Pair<Double, Double> = when (findViewById<RadioButton>(checkedButton).text.toString()) {
+                        "English" -> {
+                            textToSpeech.setLanguage(Locale.UK)
+                            textToSpeech.speak("Hello", TextToSpeech.QUEUE_FLUSH, null, null)
+                            if (Random.nextBoolean()) Pair(51.51, 0.13) else Pair(53.41, 2.99)
+                        }
+                        "Korean" -> {
+                            textToSpeech.setLanguage(Locale.KOREAN)
+                            textToSpeech.speak("안녕하세요", TextToSpeech.QUEUE_FLUSH, null, null)
+                            if (Random.nextBoolean()) Pair(37.55, 126.99) else Pair(35.21,129.07)
+                        }
+                        "Chinese" -> {
+                            textToSpeech.setLanguage(Locale.CHINESE)
+                            // Android's built in TTS doesn't support Chinese so I'll simply write out the pinyin for it
+                            textToSpeech.speak("ni hao", TextToSpeech.QUEUE_FLUSH, null, null)
+                            if (Random.nextBoolean()) Pair(39.9, 116.41) else Pair(31.23, 121.47)
+                        }
+                        else -> {
+                            Pair(0.0, 0.0)
+                        }
                     }
+                    // Source https://stackoverflow.com/questions/22704451/open-google-maps-through-intent-for-specific-location-in-android
                     val geoUri = "geo:${coordinate.first},${coordinate.second}?z=10"
                     val intent = Intent(Intent.ACTION_VIEW, Uri.parse(geoUri))
                     startActivity(intent)
